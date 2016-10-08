1730729ba10c98c5c67e1e1b978ab77ea9d630e8
Drew Neely <gpsdrew@gmail.com>
2016-10-08 08:52:44 -0400

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <input type="text">
+        <br>
+        <input type="button" value="Click to load">
+        <br>
+        <div class="content">
+        
+        </div>
+    </body>
+</html>
